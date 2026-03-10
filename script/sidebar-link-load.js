fade2a0ab87743a94b413c1b2c5eebee9f9290dd
H4wKie <lousrus@outlook.com>
2026-03-10 11:25:46 +0100

    changed the colours

--- script/sidebar-link-load.js
+++ script/sidebar-link-load.js
@@ -3,7 +3,7 @@ function sideBarLoadLinks(linkArr) {
   linkArr.forEach(element => {
     returnString += `
       <div class="side-bar-link">
-        <img src="${element.image}" alt="">
+        <img src="${element.image}" alt="" class="side-bar-link-svg">
         <h3>${element.title}</h3>
       </div>
     `;
